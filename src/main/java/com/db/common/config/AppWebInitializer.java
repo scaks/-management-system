498ba5b0f7c1d12912a44abff63174ee37a4f5f7
L M <limin65535@163.com>
2019-10-14 00:29:59 +0800

    3

--- src/main/java/com/db/common/config/AppWebInitializer.java
+++ src/main/java/com/db/common/config/AppWebInitializer.java
@@ -1,5 +1,24 @@
 package com.db.common.config;
 
-public class AppWebInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+/**
+ * Tomcat启动时加载此类，完成初始化操作
+ */
+public class AppWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
 }
